2d30b57e420da8802bbba7c874725bbb38cb2209
binraw <truvelotrobin@yahoo.fr>
2023-07-20 17:55:14 +0200

    fix btn

--- src/components/Btn/BtnGit.js
+++ src/components/Btn/BtnGit.js
@@ -3,9 +3,8 @@ import logogit from "../../assets/logogit.svg";
 function BtnGit() {
 	return (
 		<button className="btn-git">
-			<img src={logogit} alt="logo github" />
-			<a className="word-github" href="https://github.com/binraw">
-				GitHub
+			<a href="https://github.com/binraw">
+				<img src={logogit} alt="logo github" />
 			</a>
 		</button>
 	);
