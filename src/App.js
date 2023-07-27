4011be1ca86c9d2c1948057756191c561ce00785
binraw <truvelotrobin@yahoo.fr>
2023-07-27 16:39:07 +0200

    fix page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./style/style.css";
-import Home from "./pages/Home/Home";
+
 import AppRouter from "./router/AppRouter";
 
 function App() {
 	return (
 		<div>
 			<AppRouter />
-
-			<Home />
 		</div>
 	);
 }
